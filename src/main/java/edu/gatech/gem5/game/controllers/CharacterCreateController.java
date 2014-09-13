cee01476212578c25c327b03316da334246520e5
jp94 <jamespark@gatech.edu>
2014-09-12 22:41:14 -0400

    Created a menu and a skeleton controller for CharacterCreate Screen.

--- src/main/java/edu/gatech/gem5/game/controllers/CharacterCreateController.java
+++ src/main/java/edu/gatech/gem5/game/controllers/CharacterCreateController.java
@@ -1,3 +1,7 @@
+package edu.gatech.gem5.game.controllers;
+
+
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
