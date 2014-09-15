03613fafdcb9b30b7a6ade5fc7845444bdc5386f
Creston Bunch <creston@bunch.im>
2014-09-14 23:09:01 -0400

    Button to new game now appropriately changes the scene to character creation. Changed character creation screen style. Added a listener to automatically letterbox scenes when the stage changes scenes in the Application class.

--- src/main/java/edu/gatech/gem5/game/LastAdventures.java
+++ src/main/java/edu/gatech/gem5/game/LastAdventures.java
@@ -1,15 +1,16 @@
 package edu.gatech.gem5.game;
 
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.Group;
 import javafx.scene.layout.Pane;
 import javafx.scene.transform.Scale;
+import javafx.scene.text.Font;
 import javafx.stage.Stage;
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
-import javafx.scene.text.Font;
+import javafx.fxml.FXMLLoader;
 import java.net.URI;
 
 /**
@@ -20,102 +21,120 @@ import java.net.URI;
  */
 public class LastAdventures extends Application {
 
-	@Override
-	public void start(Stage stage) throws Exception {
-		Parent root = FXMLLoader.load(getClass().getResource("/Title.fxml")
-		// add resource bundle or something...
-				);
+    @Override
+    public void start(Stage stage) throws Exception {       
 
+		Parent root = FXMLLoader.load(getClass().getResource("/title.fxml"));
 		Scene scene = new Scene(root);
-		scene.getStylesheets().add("title.css");
 
-		stage.setScene(scene);
-		stage.show();
 
-		letterbox(scene, (Pane) root);
-		// stage.setFullScreen(true);
-	}
-
-	/**
-	 * @param args
-	 *            the command line arguments
-	 */
-	public static void main(String[] args) {
-		launch(args);
-	}
-
-	private void letterbox(final Scene scene, final Pane contentPane) {
-		final double initWidth = scene.getWidth();
-		final double initHeight = scene.getHeight();
-		final double ratio = initWidth / initHeight;
-
-		SizeChangeListener sizeListener = new SizeChangeListener(scene, ratio,
-				initHeight, initWidth, contentPane);
-		scene.widthProperty().addListener(sizeListener);
-		scene.heightProperty().addListener(sizeListener);
-	}
-
-	/**
-	 * Allows all elements to scale equally to the screen size.
-	 *
-	 * @author jewelsea from StackOverflow and Creston Bunch
-	 */
-	private static class SizeChangeListener implements ChangeListener<Number> {
-		private final Scene scene;
-		private final double ratio;
-		private final double initHeight;
-		private final double initWidth;
-		private final Pane contentPane;
-
-		/**
-		 * Initialize the listener.
-		 *
-		 * @param scene
-		 *            The scene being listened to.
-		 * @param ratio
-		 *            The ratio of width / height.
-		 * @param initHeight
-		 *            The initial height.
-		 * @param initWidth
-		 *            The initial width.
-		 * @param contentPane
-		 *            The content pane.
-		 */
-		public SizeChangeListener(Scene scene, double ratio, double initHeight,
-				double initWidth, Pane contentPane) {
-			this.scene = scene;
-			this.ratio = ratio;
-			this.initHeight = initHeight;
-			this.initWidth = initWidth;
-			this.contentPane = contentPane;
-		}
-
-		/**
-		 * Scale the scene to a new size.
-		 */
-		@Override
-		public void changed(ObservableValue<? extends Number> observableValue,
-				Number oldValue, Number newValue) {
-			final double newWidth = scene.getWidth();
-			final double newHeight = scene.getHeight();
-
-			double scaleFactor = (newWidth / newHeight > ratio) ? newHeight
-					/ initHeight : newWidth / initWidth;
-
-			if (scaleFactor >= 1) {
-				Scale scale = new Scale(scaleFactor, scaleFactor);
-				scale.setPivotX(0);
-				scale.setPivotY(0);
-				scene.getRoot().getTransforms().setAll(scale);
-
-				contentPane.setPrefWidth(newWidth / scaleFactor);
-				contentPane.setPrefHeight(newHeight / scaleFactor);
-			} else {
-				contentPane.setPrefWidth(Math.max(initWidth, newWidth));
-				contentPane.setPrefHeight(Math.max(initHeight, newHeight));
+		// Listen for moments when the scene changes on the stage, then
+		// re-attach the size change listeners new the new scene
+		stage.sceneProperty().addListener(new ChangeListener<Scene>() {
+			public void changed(
+				ObservableValue<? extends Scene> observable,
+				Scene oldValue,
+				Scene newValue
+			) {
+				Pane root = (Pane) newValue.getRoot();
+				letterbox(newValue, root);
 			}
-		}
+		});
 
-	}
+		// For some reason you have to show the stage before letterboxing it
+		// for the scene change listener to work.
+        stage.show();
+        stage.setScene(scene);
+
+        //stage.setFullScreen(true);
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    private void letterbox(final Scene scene, final Pane contentPane) {
+        final double initWidth  = scene.getWidth();
+        final double initHeight = scene.getHeight();
+        final double ratio      = initWidth / initHeight;
+
+        SizeChangeListener sizeListener =
+            new SizeChangeListener(
+                scene, ratio, initHeight, initWidth, contentPane
+            );
+        scene.widthProperty().addListener(sizeListener);
+        scene.heightProperty().addListener(sizeListener);
+    }
+
+    /**
+     * Allows all elements to scale equally to the screen size.
+     *
+     * @author jewelsea from StackOverflow and Creston Bunch
+     */
+    private static class SizeChangeListener implements ChangeListener<Number> {
+        private final Scene scene;
+        private final double ratio;
+        private final double initHeight;
+        private final double initWidth;
+        private final Pane contentPane;
+
+        /**
+         * Initialize the listener.
+         *
+         * @param scene The scene being listened to.
+         * @param ratio The ratio of width / height.
+         * @param initHeight The initial height.
+         * @param initWidth The initial width.
+         * @param contentPane The content pane.
+         */
+        public SizeChangeListener(
+            Scene scene,
+            double ratio,
+            double initHeight,
+            double initWidth,
+            Pane contentPane
+        ) {
+            this.scene = scene;
+            this.ratio = ratio;
+            this.initHeight = initHeight;
+            this.initWidth = initWidth;
+            this.contentPane = contentPane;
+        }
+
+        /**
+         * Scale the scene to a new size.
+         */
+        @Override
+        public void changed(
+            ObservableValue<? extends Number> observableValue,
+            Number oldValue,
+            Number newValue
+        ) {
+            final double newWidth = scene.getWidth();
+            final double newHeight = scene.getHeight();
+
+            double scaleFactor =
+                (newWidth / newHeight > ratio)
+                ? newHeight / initHeight
+                : newWidth / initWidth;
+
+            if (scaleFactor >= 1) {
+                Scale scale = new Scale(scaleFactor, scaleFactor);
+                scale.setPivotX(0);
+                scale.setPivotY(0);
+                scene.getRoot().getTransforms().setAll(scale);
+
+                contentPane.setPrefWidth(newWidth / scaleFactor);
+                contentPane.setPrefHeight(newHeight / scaleFactor);
+            } else {
+                contentPane.setPrefWidth(Math.max(initWidth, newWidth));
+                contentPane.setPrefHeight(Math.max(initHeight, newHeight));
+            }
+        }
+
+    }
 
 }
--- src/main/java/edu/gatech/gem5/game/controllers/CharacterCreateController.java
+++ src/main/java/edu/gatech/gem5/game/controllers/CharacterCreateController.java
@@ -1,7 +1,5 @@
 package edu.gatech.gem5.game.controllers;
 
-
-
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
