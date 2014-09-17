c7057ef7ad1bdc8085e134b28782374e296bacd2
Sam Blumenthal <sam.sam.42@gmail.com>
2014-09-16 20:11:03 -0400

    Gnat file working properly.

--- src/main/java/edu/gatech/gem5/game/Gnat.java
+++ src/main/java/edu/gatech/gem5/game/Gnat.java
@@ -1,6 +1,6 @@
 package edu.gatech.gem5.game;
 
-public class Gnat {
+public class Gnat extends Ship{
 
     public Gnat() {
         super("Gnat", 15, 1, 0, 1, 1, 14, 5, 2, 10000, 50, 28, 100, 0, 0, 0, 1, 1);
