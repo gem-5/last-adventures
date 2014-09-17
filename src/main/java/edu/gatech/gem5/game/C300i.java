990a87c11bb005202b1db0989c9eba85334c010d
Kyo91 <sam.sam.42@gmail.com>
2014-09-17 12:53:39 -0400

    Fixed syntax errors.

--- src/main/java/edu/gatech/gem5/game/C300i.java
+++ src/main/java/edu/gatech/gem5/game/C300i.java
@@ -7,6 +7,6 @@ package edu.gatech.gem5.game;
 public class C300i extends Ship {
 
     public C300i() {
-        super("C 300i", 25, 1, 1, 1, 1, 17, 5, 3, 3000, 75, 100 "DMW");
+        super("C 300i", 25, 1, 1, 1, 1, 17, 5, 3, 3000, 75, 100, "DMW");
     }
 }
--- src/main/java/edu/gatech/gem5/game/C600i.java
+++ src/main/java/edu/gatech/gem5/game/C600i.java
@@ -4,7 +4,7 @@ package edu.gatech.gem5.game;
  * Class for C 600i ship in Last Adventures
  * @author Sam Blumenthal
  */
-public class C600i {
+public class C600i extends Ship {
 
     public C600i() {
         super("C 600i", 25, 2, 1, 1, 1, 17, 5, 3, 3500, 75, 100, "DMW");
