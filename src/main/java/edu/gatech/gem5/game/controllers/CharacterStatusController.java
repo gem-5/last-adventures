368ab81084bfd6e29a357f104ff42d683240cbf3
jp94 <jamespark@gatech.edu>
2014-09-19 23:35:29 -0400

    Added fade-in animation to all Label objects @ CharacterStatusController.java

--- src/main/java/edu/gatech/gem5/game/controllers/CharacterStatusController.java
+++ src/main/java/edu/gatech/gem5/game/controllers/CharacterStatusController.java
@@ -3,7 +3,6 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package edu.gatech.gem5.game.controllers;
 
 import edu.gatech.gem5.game.Character;
@@ -17,12 +16,14 @@ import javafx.scene.control.Label;
 
 /**
  * FXML Controller Class
- * 
+ *
  * Shows character stats after creation.
- * 
+ *
  * @author Alex
+ * @author James Jong Han Park
  */
 public class CharacterStatusController implements Initializable {
+
     Parent root;
     @FXML
     Label playerName;
@@ -36,18 +37,33 @@ public class CharacterStatusController implements Initializable {
     Label engineerValue;
     @FXML
     Label investorValue;
-    
+
     /**
      * Initializes the controller class.
      */
     @Override
     public void initialize(URL url, ResourceBundle bundle) {
         Character player = LastAdventures.getCurrentSaveFile().getCharacter();
+
+        Label[] labels = {playerName, pilotValue, fighterValue, traderValue, engineerValue, investorValue};
+
         playerName.setText("Name: " + player.getName());
         pilotValue.setText("Pilot: " + player.getPilot());
         fighterValue.setText("Fighter: " + player.getFighter());
         traderValue.setText("Trader: " + player.getTrader());
         engineerValue.setText("Engineer: " + player.getEngineer());
         investorValue.setText("Investor: " + player.getInvestor());
+
+        FadeHandler fh;
+
+        // Sets all labels to transparent/invisible (for fade effect below)
+        for (Label label : labels) {
+            label.setOpacity(0);
+        }
+
+        // Adds fade in effects. Labels appear in order, .2 seconds per label.
+        for (int x = 0; x < labels.length; x++) {
+            fh = new FadeHandler(labels[x], x / 5.0);
+        }
     }
 }
