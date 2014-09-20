770d67c9ed3b0c6cf4670c5338b4461791afbdd1
jp94 <jamespark@gatech.edu>
2014-09-20 01:56:45 -0400

    Added transitions to Title screen @ TitleController.java, title.fxml

--- src/main/java/edu/gatech/gem5/game/controllers/TitleController.java
+++ src/main/java/edu/gatech/gem5/game/controllers/TitleController.java
@@ -3,13 +3,13 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package edu.gatech.gem5.game.controllers;
 
 import edu.gatech.gem5.game.LastAdventures;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
@@ -17,6 +17,9 @@ import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.GridPane;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
@@ -28,21 +31,27 @@ import javafx.stage.Stage;
 public class TitleController implements Initializable {
 
     @FXML
-    Parent root;
-    
+    private Parent root;
+
+    @FXML
+    private AnchorPane defaultScene;
+
+    @FXML
+    private GridPane gridPane;
+
     /**
      * Changes the scene based on the button pressed.
      */
     @FXML
     private void changeScenes(ActionEvent event) throws Exception {
         //gets this scene's stage
-        
+
         Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
-        String id = ((Button)(event.getSource())).idProperty().get();
+        String id = ((Button) (event.getSource())).idProperty().get();
         //loads the create FXML file into root
         if (id.equals("newButton")) {
             LastAdventures.createNewSaveFile();
-            root = FXMLLoader.load(getClass().getResource("/create.fxml"));                     
+            root = FXMLLoader.load(getClass().getResource("/create.fxml"));
         } else if (id.equals("continueButton")) {
             //TODO
             root = FXMLLoader.load(getClass().getResource("/title.fxml"));
@@ -50,20 +59,39 @@ public class TitleController implements Initializable {
             //TODO
             root = FXMLLoader.load(getClass().getResource("/title.fxml"));
         }
-        
+
         //sets the stage to root scene
         stage.setScene(new Scene((Pane) root));
+
     }
-    
+
     /**
      * Initialize the controller.
      *
      * @param url The location to resolve all relative paths for the root
      * object.
-     * @param resources The resources used to localize the root object.
+     * @param rb The resources used to localize the root object.
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-    }    
-    
+        // Adds fade-in animation when mouse enters the screen.
+        defaultScene.setOnMouseEntered(new EventHandler<MouseEvent>() {
+
+            @Override
+            public void handle(MouseEvent mouseEvent) {
+                new FadeHandler(gridPane);
+
+            }
+        });
+
+        // Adds fade-out animation when mouse leaves the screen.
+        defaultScene.setOnMouseExited(new EventHandler<MouseEvent>() {
+
+            @Override
+            public void handle(MouseEvent mouseEvent) {
+                new FadeHandler(gridPane, .5, 0, 1, 0);
+            }
+        });
+    }
+
 }
