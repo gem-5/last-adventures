2c10c54cfdd6e85c47a35d6f6b65dd802f30bbed
jp94 <jamespark@gatech.edu>
2014-09-19 23:43:06 -0400

    Changed Button object names @ TitleController.java, Title.fxml Reason: Button id called "new" conflicts when calling from TitleController.java, because Java reserves "new". Changed to "newButton", etc.

--- src/main/java/edu/gatech/gem5/game/controllers/TitleController.java
+++ src/main/java/edu/gatech/gem5/game/controllers/TitleController.java
@@ -3,6 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+
 package edu.gatech.gem5.game.controllers;
 
 import edu.gatech.gem5.game.LastAdventures;
@@ -22,47 +23,47 @@ import javafx.stage.Stage;
 /**
  *
  * @author Creston Bunch
+ * @author James Jong Han Park
  */
 public class TitleController implements Initializable {
 
     @FXML
     Parent root;
-
+    
     /**
      * Changes the scene based on the button pressed.
      */
     @FXML
     private void changeScenes(ActionEvent event) throws Exception {
         //gets this scene's stage
-
+        
         Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
-        String id = ((Button) (event.getSource())).idProperty().get();
+        String id = ((Button)(event.getSource())).idProperty().get();
         //loads the create FXML file into root
-        if (id.equals("new")) {
+        if (id.equals("newButton")) {
             LastAdventures.createNewSaveFile();
-            root = FXMLLoader.load(getClass().getResource("/create.fxml"));
-        } else if (id.equals("continue")) {
+            root = FXMLLoader.load(getClass().getResource("/create.fxml"));                     
+        } else if (id.equals("continueButton")) {
             //TODO
             root = FXMLLoader.load(getClass().getResource("/title.fxml"));
-        } else if (id.equals("load")) {
+        } else if (id.equals("loadButton")) {
             //TODO
             root = FXMLLoader.load(getClass().getResource("/title.fxml"));
         }
+        
         //sets the stage to root scene
         stage.setScene(new Scene((Pane) root));
     }
-
+    
     /**
-     * Initialize the controller. Does nothing because there's nothing to
-     * initialize.
+     * Initialize the controller.
      *
      * @param url The location to resolve all relative paths for the root
      * object.
      * @param resources The resources used to localize the root object.
      */
     @Override
-    public void initialize(URL url, ResourceBundle resources) {
-        // do nothing
-    }
-
+    public void initialize(URL url, ResourceBundle rb) {
+    }    
+    
 }
