7735b636c4edf5b11d7c53cdd24de7118b3999ab
Jack Mueller <jmueller30@gatech.edu>
2014-09-22 01:02:55 -0400

    added market screen to go to after display universe screen

--- src/main/java/edu/gatech/gem5/game/controllers/DisplayUniverseController.java
+++ src/main/java/edu/gatech/gem5/game/controllers/DisplayUniverseController.java
@@ -6,13 +6,11 @@ import edu.gatech.gem5.game.Universe;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.collections.ObservableList;
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
@@ -41,7 +39,7 @@ public class DisplayUniverseController implements Initializable {
     public void changeScenes(MouseEvent event) throws Exception {
         Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
 
-        root = FXMLLoader.load(getClass().getResource("/title.fxml"));
+        root = FXMLLoader.load(getClass().getResource("/market.fxml"));
      
         stage.setScene(new Scene((Pane) root));
     }
