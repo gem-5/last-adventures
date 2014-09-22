9d09a98fdcb9ae6b7c079b88e40f982b9d48bacf
Kyo91 <sam.sam.42@gmail.com>
2014-09-22 01:28:51 -0400

    Added names to Tech & Resource Levels

--- src/main/java/edu/gatech/gem5/game/Planet.java
+++ src/main/java/edu/gatech/gem5/game/Planet.java
@@ -10,25 +10,36 @@ import java.util.Random;
 /**
  *
  * @author Jack
-        
+
  */
 class Planet {
     private int techLevel;
     private final int resource;
     Company[] companies;
-    
+    private static final String[] techLevels = {"Pre-Agriculture", "Agriculture",
+                                                "Medieval", "Renaissance", "Early Industrial",
+                                                "Industrial", "Post-Industrial", "Hi-Tech"};
+
+    // temporary, we'll probably want different ones based on our goods.
+    // Also, if we follow the original, then it should be weighted a bit towards NOSPECIALRESOURCES
+    private static final String[] resourceLevels = {"NOSPECIALRESOURCES", "MINERALRICH",
+                                                    "MINERALPOOR", "DESERT", "LOTSOFWATER",
+                                                    "RICHSOIL", "POORSOIL", "RICHFAUNA",
+                                                    "LIFELESS", "WEIRDMUSHROOMS", "LOTSOFHERBS",
+                                                    "ARTISTIC", "WARLIKE"};
+
     public Planet() {
         techLevel = new Random().nextInt(8);
         resource = new Random().nextInt(13);
         companies = null;
     }
-    
+
     @Override
     public String toString() {
         String result = "";
-        result += "Tech Level: " + techLevel;
+        result += "Tech Level: " + techLevels[techLevel] + " {" + techLevel + "}";
         result += "\n";
-        result += "Special Resource " + resource;
+        result += "Special Resource: " + resourceLevels[resource] + " {" + resource + "}";
         return result;
     }
 }
