395d05c2ac05b40a28480fc1d2f8a7ad9970dca6
jp94 <jamespark@gatech.edu>
2014-10-10 23:57:27 -0400

    Removed all unused imports @ TitleController.java

--- src/main/java/edu/gatech/gem5/game/controllers/TitleController.java
+++ src/main/java/edu/gatech/gem5/game/controllers/TitleController.java
@@ -1,25 +1,11 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package edu.gatech.gem5.game.controllers;
 
 import edu.gatech.gem5.game.LastAdventures;
-import edu.gatech.gem5.game.SaveFile;
 import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Node;
 import javafx.scene.Parent;
-import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
-import javafx.scene.layout.Pane;
-import javafx.stage.Stage;
 
 /**
  *
