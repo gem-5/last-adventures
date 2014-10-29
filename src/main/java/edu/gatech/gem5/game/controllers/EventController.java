d6060a5c856f97023da1aacfd5e612ceb5d232e2
jp94 <jamespark@gatech.edu>
2014-10-28 23:24:09 -0400

    Removed debug messages @ EventController.java

--- src/main/java/edu/gatech/gem5/game/controllers/EventController.java
+++ src/main/java/edu/gatech/gem5/game/controllers/EventController.java
@@ -2,16 +2,12 @@ package edu.gatech.gem5.game.controllers;
 
 import edu.gatech.gem5.game.Character;
 import edu.gatech.gem5.game.Event;
-import edu.gatech.gem5.game.EventEffectParser;
 import edu.gatech.gem5.game.LastAdventures;
-import java.net.URL;
 import java.util.Map;
 import java.util.Map.Entry;
-import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
-import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 
@@ -50,19 +46,18 @@ public class EventController extends Controller {
         });
         
         Map<String, Map> effects = event.getEffects();
-        EventEffectParser parser = new EventEffectParser();
         for(Entry<String, Map> entry : effects.entrySet()) {
-            if(parser.isSupportedEffect(entry.getKey())) {
-                parser.doEffect(entry.getKey(), entry.getValue());
+            /*if(supportedEffect(entry)) {
+                doEffect(entry.getKey(), entry.getValue());
             } else {
                 System.err.println("Unsupported Effect in " + this.event.getKey());
-            }
-            //System.out.println("hello" + entry.getKey());
+            }*/
+            // System.out.println("hello" + entry.getKey());
             message.setText(event.getSuccessMessage());
             if(entry.getKey().equals("money")) {
                 Character character = LastAdventures.getCurrentSaveFile().getCharacter();
                 double themoney = (double) (Double) (entry.getValue().get("min"));
-                character.setMoney(character.getMoney() + (int) themoney);
+                character.setMoney(Math.max(0, character.getMoney() + (int) themoney));
             }
         }
     }
