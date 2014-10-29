004a53fcde393b99c585b447a0ee962c8ea9e55c
jp94 <jamespark@gatech.edu>
2014-10-28 22:34:24 -0400

    Added dock to the game

--- src/main/java/edu/gatech/gem5/game/controllers/DockController.java
+++ src/main/java/edu/gatech/gem5/game/controllers/DockController.java
@@ -1,31 +1,30 @@
 package edu.gatech.gem5.game.controllers;
 
-import edu.gatech.gem5.game.LastAdventures;
 import edu.gatech.gem5.game.Data;
+import edu.gatech.gem5.game.Character;
+import edu.gatech.gem5.game.LastAdventures;
 import edu.gatech.gem5.game.Planet;
 import edu.gatech.gem5.game.SaveFile;
 import edu.gatech.gem5.game.Ship;
-import edu.gatech.gem5.game.data.ShipType;
-import edu.gatech.gem5.game.ui.ShipBar;
+import edu.gatech.gem5.game.data.ShieldType;
+import edu.gatech.gem5.game.data.WeaponType;
+import edu.gatech.gem5.game.ui.UpgradeBar;
 import java.util.Map;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
-import javafx.scene.control.ToggleGroup;
-import javafx.scene.layout.AnchorPane;
 
 /**
  *
- * @author James
- * @author Alex
+ * @author James Park
  */
 public class DockController extends Controller {
-    @FXML
-    AnchorPane root;
+
     @FXML
     Button universe;
     @FXML
@@ -35,12 +34,8 @@ public class DockController extends Controller {
     @FXML
     private Label shipInfo;
     @FXML
-    private ListView<ShipBar> buyShips;
-
-    private ToggleGroup shipGroup;
-
-    private Planet planet;
-    private Ship currentShip;
+    private ListView<UpgradeBar> buyUpgradeBarList;
+    private final Planet planet;
 
     public static final String SHIPYARD_VIEW_FILE = "/fxml/dock.fxml";
 
@@ -49,44 +44,15 @@ public class DockController extends Controller {
 
         SaveFile save = LastAdventures.getCurrentSaveFile();
         planet = save.getPlanet();
-        currentShip = save.getCharacter().getShip();
-        shipGroup = new ToggleGroup();
-
-        fillLabels();
-        buildShipsList();
+        refreshLabels();
     }
 
-    @FXML
-    public void makeSale(ActionEvent event) throws Exception {
-        // TODO:
-        // K, here's what needs to happen to beautify this:
-        // ShipBar needs to extend javafx.scene.control.Toggle,
-        // and then we can use ToggleGroup.getSelectedToggle()
-        // rather than this shit
-        SaveFile save = LastAdventures.getCurrentSaveFile();
-        Map<String, ShipType> ships = Data.SHIPS.get();
-        for (ShipBar bar : buyShips.getItems()) {
-            if (bar.isSelected()) {
-                ShipType ship = ships.get(bar.getKey());
-                int playerCash = save.getCharacter().getMoney();
-                int price = ship.getPrice();
-                if (playerCash < price) {
-                    // TODO: warning: not enough money
-                } else {
-                    // TODO: use a dedicated class to facilitate ship upgrades
-                    // the same way Transaction  is used for goods
-                    Ship newShip = new Ship(ship);
-                    save.getCharacter().setShip(newShip);
-                    save.getCharacter().setMoney(playerCash - price);
-                    // update labels
-                    fillLabels();
-                }
-                // TODO: don't use a for loop here (see above)
-                break;
-            }
-        }
-    }
-
+    /**
+     * Go back to the planet screen.
+     *
+     * @param event A button press attempting to change scenes
+     * @throws Exception propagates javaFX exceptions
+     */
     @FXML
     public void goBack(ActionEvent event) throws Exception {
         LastAdventures.swap(new PlanetController());
@@ -97,23 +63,131 @@ public class DockController extends Controller {
         this.lblCash.setText(
                 ((Integer) save.getCharacter().getMoney()).toString()
         );
+
+        // Pull the ship's information
         Ship s = save.getCharacter().getShip();
         this.shipInfo.setText(s.toString());
+
+        // Reset the error label. When this method is called, we assert that there will be no errors..
         errorLabel.setText("");
     }
 
-    private void buildShipsList() {
-        ObservableList<ShipBar> lstShips = FXCollections.observableArrayList();
-        Map<String, ShipType> ships = Data.SHIPS.get();
-        for (String s : planet.getShips()) {
-            ShipType ship = ships.get(s);
-            ShipBar u = new ShipBar();
-            u.setKey(s);
-            u.setText(ship.getName());
-            u.setPrice(ship.getPrice() - currentShip.getNetWorth());
-            u.setToggleGroup(shipGroup);
-            lstShips.add(u);
+    // TODO Prevent players from purchasing, when slots are full.
+    private void buildBuyUpgradesList() {
+
+        ObservableList<UpgradeBar> upgradeItemsList = FXCollections.observableArrayList();
+
+        // Pull all weapons and shields list
+        Map<String, WeaponType> weapons = Data.WEAPONS.get();
+        Map<String, ShieldType> shields = Data.SHIELDS.get();
+
+        // Add weapons from a particular planet to the purchase list.
+        for (String s : planet.getWeapons()) {
+            WeaponType weapon = weapons.get(s);
+            UpgradeBar upgradeWeaponBar = new UpgradeBar();
+            upgradeWeaponBar.setKey(s);
+            upgradeWeaponBar.setText(weapon.getName());
+            upgradeWeaponBar.setPrice(weapon.getPrice());
+            upgradeWeaponBar.setOnAction(new BuyWeaponHandler(weapon));
+            upgradeItemsList.add(upgradeWeaponBar);
         }
-        buyShips.setItems(lstShips);
+
+        // Add shields from a particular planet to the purchase list.
+        for (String s : planet.getShields()) {
+            ShieldType shield = shields.get(s);
+            UpgradeBar shieldUpgradeBar = new UpgradeBar();
+            shieldUpgradeBar.setKey(s);
+            shieldUpgradeBar.setText(shield.getName());
+            shieldUpgradeBar.setPrice(shield.getPrice());
+            shieldUpgradeBar.setOnAction(new BuyShieldHandler(shield));
+            upgradeItemsList.add(shieldUpgradeBar);
+        }
+
+        // Update upgradeItemsList
+        buyUpgradeBarList.setItems(upgradeItemsList);
+    }
+
+    /**
+     * Helper method to refresh all of the required label. Updates purchases
+     * list & player's data.
+     */
+    private void refreshLabels() {
+        buildBuyUpgradesList();
+        fillLabels();
+    }
+
+    // TODO (James) I was told to do this way, because of the way other classes are set up. There are definitely better ways to do this.
+    /**
+     * Handles an event for when a player initiates a weapon purchase.
+     */
+    private class BuyWeaponHandler implements EventHandler<ActionEvent> {
+
+        private final WeaponType weaponType;
+
+        public BuyWeaponHandler(WeaponType weaponType) {
+            this.weaponType = weaponType;
+        }
+
+        /**
+         * Processes transaction, and appropriate weapon allocation.
+         *
+         * @param t An ActionEvent
+         */
+        @Override
+        public void handle(ActionEvent t) {
+
+            Character player = LastAdventures.getCurrentSaveFile().getCharacter();
+            Ship playerShip = player.getShip();
+
+            // Check for weapon slot availability for a player's ship (edge case)
+            if (playerShip.getType().getWeaponSlots() <= playerShip.getWeaponList().size()) {
+                errorLabel.setText("No more weapon slots remaining for the ship.");
+                return;
+            }
+
+            // Process weapon purchase
+            playerShip.getWeaponList().add(weaponType);
+            player.setMoney(player.getMoney() - weaponType.getPrice());
+
+            // Refresh list & money count
+            refreshLabels();
+        }
+    }
+
+    /**
+     * Handles an event for when a player initiates a shield purchase.
+     */
+    private class BuyShieldHandler implements EventHandler<ActionEvent> {
+
+        private final ShieldType shieldType;
+
+        public BuyShieldHandler(ShieldType shieldType) {
+            this.shieldType = shieldType;
+        }
+
+        /**
+         * Processes transaction, and appropriate shield allocation.
+         *
+         * @param t An ActionEvent
+         */
+        @Override
+        public void handle(ActionEvent t) {
+
+            Character player = LastAdventures.getCurrentSaveFile().getCharacter();
+            Ship playerShip = player.getShip();
+
+            // Check for weapon slot availability for a player's ship (edge case)
+            if (playerShip.getType().getShieldSlots() <= playerShip.getShieldList().size()) {
+                errorLabel.setText("No more weapon slots remaining for the ship.");
+                return;
+            }
+
+            // Process shield purchase
+            playerShip.getShieldList().add(shieldType);
+            player.setMoney(player.getMoney() - shieldType.getPrice());
+
+            // Refresh list & money count
+            refreshLabels();
+        }
     }
 }
