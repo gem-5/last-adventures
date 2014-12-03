f34ea204456f9c6f3597556b19096a625d725e14
jp94 <jamespark@gatech.edu>
2014-12-02 23:40:19 -0500

    Added animation to title screen

--- src/main/java/edu/gatech/gem5/game/controllers/TitleController.java
+++ src/main/java/edu/gatech/gem5/game/controllers/TitleController.java
@@ -1,7 +1,12 @@
 package edu.gatech.gem5.game.controllers;
 
+import edu.gatech.gem5.game.animation.FadeHandler;
 import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
 
 /**
  *
@@ -9,10 +14,23 @@ import javafx.fxml.FXML;
  * @author James Jong Han Park
  */
 public class TitleController extends AbstractController {
+
     /**
      * The path that the constructor will take.
      */
     public static final String TITLE_VIEW_FILE = "/fxml/title.fxml";
+
+    /**
+     * Holds buttons.
+     */
+    @FXML
+    private GridPane gridPane;
+
+    /**
+     * The entire screen.
+     */
+    @FXML
+    private BorderPane defaultScene;
 
     /**
      * Construct the title controller.
@@ -20,13 +38,35 @@ public class TitleController extends AbstractController {
     public TitleController() {
         // load the view or throw an exception
         super(TITLE_VIEW_FILE);
+        // Set opacity 0 initially, so that animation will run smoothly.
+        gridPane.setOpacity(0);
+
+        // Adds fade-in animation when mouse enters the screen.
+        defaultScene.setOnMouseEntered(new EventHandler<MouseEvent>() {
+
+            @Override
+            public void handle(MouseEvent mouseEvent) {
+                new FadeHandler(gridPane);
+
+            }
+        });
+
+        // Adds fade-out animation when mouse leaves the screen.
+        defaultScene.setOnMouseExited(new EventHandler<MouseEvent>() {
+
+            @Override
+            public void handle(MouseEvent mouseEvent) {
+                new FadeHandler(gridPane, 0, .5, 1, 0);
+            }
+        });
+
     }
 
     /**
      * Move to the new game scene.
-     * 
+     *
      * @param event A button press
-     * @throws Exception 
+     * @throws Exception
      */
     @FXML
     private void startNewGame(ActionEvent event) throws Exception {
@@ -35,9 +75,9 @@ public class TitleController extends AbstractController {
 
     /**
      * Move to the continue game scene.
-     * 
+     *
      * @param event A button press
-     * @throws Exception 
+     * @throws Exception
      */
     @FXML
     private void continueGame(ActionEvent event) throws Exception {
@@ -48,7 +88,7 @@ public class TitleController extends AbstractController {
      * Move to the load game scene.
      *
      * @param event A button press
-     * @throws Exception 
+     * @throws Exception
      */
     @FXML
     private void loadGame(ActionEvent event) throws Exception {
